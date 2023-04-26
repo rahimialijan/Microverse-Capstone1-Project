f72eaa5d983584b8e99ba59aa4e4ca8450553b83
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-04-26 14:05:53 +0500

    Fix the all linter errors

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-const hamburger=document.querySelector('.hamburger');
-const navMenu=document.querySelector('.nav-menu');
-
-hamburger.addEventListener('click', ()=>{
+hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll('.nav-list').forEach(list =>list.addEventListener(('click'), ()=>{
+document.querySelectorAll('.nav-list').forEach((list) => list.addEventListener(('click'), () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
-}))
-
+}));
--- main.js
+++ main.js
@@ -1,53 +1,55 @@
-const featureCards=document.querySelector('.feature-container');
+const featureCards = document.querySelector('.feature-container');
 
-const dataCard=[
+const dataCard = [
   {
-    id:1,
-    name:'Alijan Rahimi',
-    skill:'Frontend Developer',
-    details:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
-    image:'images/alijan.png',
+    id: 1,
+    name: 'Alijan Rahimi',
+    skill: 'Frontend Developer',
+    details: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
+    image: 'images/alijan.png',
   },
   {
-    id:2,
-    name:'Mohammad Amin Rafi',
-    skill:'Full Stack Developer',
-    details:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
-    image:'images/alijan.png',
+    id: 2,
+    name: 'Mohammad Amin Rafi',
+    skill: 'Full Stack Developer',
+    details: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
+    image: 'images/alijan.png',
   },
   {
-    id:3,
-    name:'Jawad Nazari',
-    skill:'Full Stack Developer',
-    details:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
-    image:'images/alijan.png',
+    id: 3,
+    name: 'Jawad Nazari',
+    skill: 'Full Stack Developer',
+    details: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
+    image: 'images/alijan.png',
   },
   {
-    id:4,
-    name:'Abdul Hadi Haider',
-    skill:'Frontend Developer',
-    details:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
-    image:'images/alijan.png',
+    id: 4,
+    name: 'Abdul Hadi Haider',
+    skill: 'Frontend Developer',
+    details: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
+    image: 'images/alijan.png',
   },
   {
-    id:5,
-    name:'Masouma Jaffari',
-    skill:'Full Stack Developer',
-    details:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
-    image:'images/alijan.png',
+    id: 5,
+    name: 'Masouma Jaffari',
+    skill: 'Full Stack Developer',
+    details: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
+    image: 'images/alijan.png',
   },
   {
-    id:6,
-    name:'Alijan Rahimi',
-    skill:'Full Stack Developer',
-    details:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
-    image:'images/alijan.png',
+    id: 6,
+    name: 'Alijan Rahimi',
+    skill: 'Full Stack Developer',
+    details: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
+    image: 'images/alijan.png',
   },
 ];
 
-function generatCards(){
-  featureCards.innerHTML=dataCard.map((card)=>{
-    const {id,name,skill,details,image}=card;
+function generatCards() {
+  featureCards.innerHTML = dataCard.map((card) => {
+    const {
+      id, name, skill, details, image,
+    } = card;
     return `
     <div class="feature-card" id="card${id}">
       <div class="img-box">
@@ -60,7 +62,7 @@ function generatCards(){
         <p>${details}</p>
       </div>
     </div>
-    `
+    `;
   }).join('');
 }
-generatCards()
+generatCards();
